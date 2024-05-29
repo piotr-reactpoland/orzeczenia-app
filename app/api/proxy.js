fc8cf08ac372522a0fae4985d4b8a4ed376d70e9
p-miszczuk <piotr.miszczuk@reactpoland.com>
2024-05-29 19:13:02 +0200

    fix

--- app/api/proxy.js
+++ app/api/proxy.js
@@ -2,8 +2,8 @@ import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   try {
-    // const response = await fetch("http://3.16.160.92/find");
-    // const data = await response.json();
+    const response = await fetch("http://localhost:8001/find");
+    const data = await response.json();
     res.status(200).json({ dziala: "lol" });
   } catch (error) {
     console.error("Wystąpił błąd:", error);
