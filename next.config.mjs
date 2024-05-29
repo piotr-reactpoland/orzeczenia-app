04cff1370dd4eb95b8197ce248f31df8c7a3452e
p-miszczuk <piotr.miszczuk@reactpoland.com>
2024-05-29 16:34:34 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const isTest = process.env.NODE_ENV === "test";
-console.log("🚀 ~ isTest:", isTest);
+const isLocal = process.env.NODE_ENV === "local";
+console.log("🚀 ~ isTest:", isLocal);
 
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    FETCH_URL: isTest
-      ? "http://3.16.160.92/find"
-      : "http://localhost:8001/find",
+    FETCH_URL: isLocal
+      ? "http://localhost:8001/find"
+      : "http://3.16.160.92/find",
   },
 };
 
