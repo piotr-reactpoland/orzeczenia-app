7616a4846221e4effc2898fc622859b6fdfffa31
p-miszczuk <piotr.miszczuk@reactpoland.com>
2024-05-29 19:19:12 +0200

    fix

--- app/api/proxy.js
+++ app/api/proxy.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        question,
+        question: "amfetamina",
       }),
     });
     const data = await response.json();
