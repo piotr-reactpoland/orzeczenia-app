c55b281342eba3a018e5f8dd04cfc7abd5013628
p-miszczuk <piotr.miszczuk@reactpoland.com>
2024-08-11 15:04:12 +0200

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isLocal = process.env.NODE_ENV === "local";
-const url = "3.17.27.203";
+const url = "18.216.107.92";
 
 const nextConfig = {
   reactStrictMode: false,
