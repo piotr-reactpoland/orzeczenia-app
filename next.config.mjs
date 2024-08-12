0af44f9f4ff1b8e2b47874f9822aa8b423aaf3d4
p-miszczuk <piotr.miszczuk@interia.pl>
2024-08-12 14:59:54 +0200

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isLocal = process.env.NODE_ENV === "local";
-const url = "18.216.107.92";
+const url = "12.12.12.12";
 
 const nextConfig = {
   reactStrictMode: false,
