06eafd3ede694e87df0e65dc1ca6fcd7fa56c369
p-miszczuk <piotr.miszczuk@interia.pl>
2024-08-30 12:45:49 +0200

    server fail

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isLocal = process.env.NODE_ENV === "local";
-const url = "3.19.255.79";
+const url = "3.14.86.72";
 const nextConfig = {
   reactStrictMode: false,
   env: {
